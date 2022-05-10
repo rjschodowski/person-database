440696046152dc01034da895b5a925a55a19ad19
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2022-05-10 16:58:22 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include<stdio.h>
+#include "person.hpp"
+using namespace std; 
+
+int main() {
+  
+}
---
+++ person.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include<stdio.h>
+using namespace std;
+
+class person {
+    private:        
+    string lastName;
+    string firstName;
+    int age;
+    long socSecNum;
+    string address;
+};
